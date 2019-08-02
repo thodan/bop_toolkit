3dd0cedf1bfc87900ce22f6591657d3df4ff4b8f
Tomas Hodan <tom.hodan@gmail.com>
2019-08-02 12:49:57 +0200

    Minor change in the wrapper of the C++ renderer.

--- bop_toolkit_lib/renderer_cpp.py
+++ bop_toolkit_lib/renderer_cpp.py
@@ -54,8 +54,8 @@ class RendererCpp(renderer.Renderer):
 
   def render_object(self, obj_id, R, t, fx, fy, cx, cy):
     """See base class."""
-    R_l = map(float, R.flatten().tolist())
-    t_l = map(float, t.flatten().tolist())
+    R_l = R.astype(np.float32).flatten().tolist()
+    t_l = t.astype(np.float32).flatten().tolist()
     self.renderer.render_object(obj_id, R_l, t_l, fx, fy, cx, cy)
     rgb = self.renderer.get_color_image(obj_id)
     depth = self.renderer.get_depth_image(obj_id).astype(np.float32)
