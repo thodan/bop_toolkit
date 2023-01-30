ff0e957d8f0b836884b3e7c188a8fdae69d4d09d
Martin Sundermeyer <Martin.Sundermeyer@dlr.de>
2023-01-30 09:54:27 +0100

    Improve integration with BlenderProc

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='bop_toolkit_lib',
     version='1.0',
     packages=find_packages(exclude=('docs')),
-    install_requires=["pytz"],
+    install_requires=["pytz", "vispy>=0.6.5", "PyOpenGL==3.1.0"],
     author='Tomas Hodan, Martin Sundermeyer',
     author_email='tom.hodan@gmail.com, Martin.Sundermeyer@dlr.de',
     license='MIT license',
