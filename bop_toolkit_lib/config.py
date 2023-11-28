d307dbfc4d67de83e49d764660d029aaa2947492
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-11-28 12:17:28 +0100

    set paths for happypose

--- bop_toolkit_lib/config.py
+++ bop_toolkit_lib/config.py
@@ -5,28 +5,26 @@
 
 import os
 
-
 ######## Basic ########
 
+HAPPYPOSE_DATA_DIR = os.environ["HAPPYPOSE_DATA_DIR"]
+
 # Folder with the BOP datasets.
-if 'BOP_PATH' in os.environ:
-  datasets_path = os.environ['BOP_PATH']
-else:
-  datasets_path = r'/path/to/bop/datasets'
+datasets_path = os.path.join(HAPPYPOSE_DATA_DIR, "bop_datasets")
 
 # Folder with pose results to be evaluated.
-results_path = r'/path/to/folder/with/results'
+results_path = os.path.join(HAPPYPOSE_DATA_DIR, "results")
 
 # Folder for the calculated pose errors and performance scores.
-eval_path = r'/path/to/eval/folder'
+eval_path = os.path.join(HAPPYPOSE_DATA_DIR, "bop_eval_outputs")
 
 ######## Extended ########
 
 # Folder for outputs (e.g. visualizations).
-output_path = r'/path/to/output/folder'
+output_path = r"/path/to/output/folder"
 
 # For offscreen C++ rendering: Path to the build folder of bop_renderer (github.com/thodan/bop_renderer).
-bop_renderer_path = r'/path/to/bop_renderer/build'
+bop_renderer_path = r"/path/to/bop_renderer/build"
 
 # Executable of the MeshLab server.
-meshlab_server_path = r'/path/to/meshlabserver.exe'
+meshlab_server_path = r"/path/to/meshlabserver.exe"
