58ef134b276763a82b3485433bc38fd6d9114c8a
masundermeyer <msundermeyer@google.com>
2025-01-27 19:22:47 +0100

    update xyz scene ids

--- bop_toolkit_lib/dataset_params.py
+++ bop_toolkit_lib/dataset_params.py
@@ -483,9 +483,9 @@ def get_split_params(datasets_path, dataset_name, split, split_type=None):
     elif dataset_name == "xyzibd":
         sensor_modalities_have_separate_annotations = {"photoneo": False, "xyz": False, "realsense": False}
         p["im_modalities"] = {"photoneo": ["gray", "depth"], "xyz": ["gray", "depth"], "realsense": ["rgb", "depth"]}
-        val_scene_ids = [0, 5, 10, 18, 23, 28, 33, 38, 46, 51, 56, 61, 69, 74, 79]
+        val_scene_ids = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 54, 60, 65, 70]
         p["scene_ids"] = {
-            "test": [i for i in range(84) if i not in val_scene_ids],
+            "test": [i for i in range(1, 75) if i not in val_scene_ids],
             "val": val_scene_ids,
             "train": list(range(45)),
         }[split]
