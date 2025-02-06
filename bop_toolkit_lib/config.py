10b97f4682151a2720431e2d694889e646434f49
Martin Sundermeyer <msundermeyer@gmx.de>
2025-02-06 16:46:16 +0100

    Update config.py

--- bop_toolkit_lib/config.py
+++ bop_toolkit_lib/config.py
@@ -15,15 +15,15 @@ else:
     datasets_path = r"/path/to/bop/datasets"
 
 # Folder with pose results to be evaluated.
-results_path = r"/tmp"
+results_path = r"/path/to/folder/with/results"
 
 # Folder for the calculated pose errors and performance scores.
-eval_path = r"/tmp"
+eval_path = r"/path/to/eval/folder"
 
 ######## Extended ########
 
 # Folder for outputs (e.g. visualizations).
-output_path = r"/tmp"
+output_path = r"/path/to/output/folder"
 
 # For offscreen C++ rendering: Path to the build folder of bop_renderer (github.com/thodan/bop_renderer).
 bop_renderer_path = r"/path/to/bop_renderer/build"
