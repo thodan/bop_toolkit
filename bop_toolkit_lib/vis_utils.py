1d58fef1788bb63f1b53600fb73ea674123d0670
kirilllzaitsev <kzaitsev88@gmail.com>
2025-10-22 12:27:46 +0200

    add calc_mask_visib_percent

--- bop_toolkit_lib/vis_utils.py
+++ bop_toolkit_lib/vis_utils.py
@@ -1,6 +1,15 @@
 import copy
-import cv2
 
+import cv2
+import numpy as np
+
+
+def calc_mask_visib_percent(mask_visib, valid_mask):
+    total_pixels = np.sum(valid_mask)
+    visible_pixels = np.sum(mask_visib)
+    percent_visible = (visible_pixels / total_pixels) * 100
+    return percent_visible
+    
 
 def draw_text_in_ul(
     rgb, extra_text, size=1, thickness=3, start_pos=(10, 30), color=(255, 0, 0)
