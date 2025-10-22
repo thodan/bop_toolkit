e6bedc90006589aea2f650da131e5d29833204ae
kirilllzaitsev <kzaitsev88@gmail.com>
2025-10-22 13:05:23 +0200

    add pose_dict_to_mat

--- bop_toolkit_lib/vis_utils.py
+++ bop_toolkit_lib/vis_utils.py
@@ -9,7 +9,7 @@ def calc_mask_visib_percent(mask_visib, valid_mask):
     visible_pixels = np.sum(mask_visib)
     percent_visible = (visible_pixels / total_pixels) * 100
     return percent_visible
-    
+
 
 def draw_text_in_ul(
     rgb, extra_text, size=1, thickness=3, start_pos=(10, 30), color=(255, 0, 0)
@@ -26,3 +26,10 @@ def draw_text_in_ul(
     )
 
     return rgb
+
+
+def pose_dict_to_mat(pose):
+    pose_mat = np.eye(4)
+    pose_mat[:3, :3] = pose["R"]
+    pose_mat[:3, 3] = pose["t"].squeeze()
+    return pose_mat
