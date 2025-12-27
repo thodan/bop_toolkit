d6d046d788cc143b99049106e96d7532d80b9f07
kirilllzaitsev <kzaitsev88@gmail.com>
2025-12-27 12:18:14 +0100

    v:upd gt setup

--- scripts/vis_poses.py
+++ scripts/vis_poses.py
@@ -220,17 +220,28 @@ def main(args):
     colors_path = os.path.join(os.path.dirname(visualization.__file__), "colors.json")
     colors = inout.load_json(colors_path)
 
-    result_filename = args.result_filename
-    
-    misc.log("Processing: " + result_filename)
+    if args.mode == "gt":
+        dataset, split, split_type = (
+            args.dataset,
+            args.dataset_split,
+            args.dataset_split_type,
+        )
+    else:
+        result_filename = args.result_filename
 
-    # Parse info about the method and the dataset from the filename.
-    result_name, method, dataset, split, split_type, _ = inout.parse_result_filename(result_filename)
+        misc.log("Processing: " + result_filename)
+
+        # Parse info about the method and the dataset from the filename.
+        result_name, method, dataset, split, split_type, _ = (
+            inout.parse_result_filename(result_filename)
+        )
 
     #######################
     # hot3d specific checks
     if dataset == "hot3d" and not htt_available:
-        raise ImportError("Missing hand_tracking_toolkit dependency, mandatory for HOT3D dataset.")
+        raise ImportError(
+            "Missing hand_tracking_toolkit dependency, mandatory for HOT3D dataset."
+        )
 
     if dataset == "hot3d" and args.renderer_type != "htt":
         raise ValueError("'htt' renderer_type is mandatory for HOT3D dataset.")
@@ -250,19 +261,31 @@ def main(args):
     model_type = "eval"
     dp_model = dataset_params.get_model_params(args.datasets_path, dataset, model_type)
 
-    # Load pose estimates.
-    misc.log("Loading pose estimates...")
-    ests = inout.load_bop_results(os.path.join(args.results_path, result_filename))
+    if args.mode == "gt":
+        # Subset of images for which the ground-truth poses will be rendered.
+        if args.targets_filename is not None:
+            targets = inout.load_json(
+                os.path.join(dp_split["base_path"], args.targets_filename)
+            )
+            scene_im_ids = {}
+            for target in targets:
+                scene_im_ids.setdefault(target["scene_id"], set()).add(target["im_id"])
+        else:
+            scene_im_ids = None
+    else:
+        # Load pose estimates.
+        misc.log("Loading pose estimates...")
+        ests = inout.load_bop_results(os.path.join(args.results_path, result_filename))
 
-    # Organize the pose estimates by scene, image and object.
-    misc.log("Organizing pose estimates...")
-    ests_org = {}
-    for est in ests:
-        if est['scene_id'] not in [48]:
-            continue
-        ests_org.setdefault(est["scene_id"], {}).setdefault(
-            est["im_id"], {}
-        ).setdefault(est["obj_id"], []).append(est)
+        # Organize the pose estimates by scene, image and object.
+        misc.log("Organizing pose estimates...")
+        ests_org = {}
+        for est in ests:
+            if est["scene_id"] not in [48]:
+                continue
+            ests_org.setdefault(est["scene_id"], {}).setdefault(
+                est["im_id"], {}
+            ).setdefault(est["obj_id"], []).append(est)
 
     # Rendering mode.
     renderer_modalities = []
@@ -277,12 +300,20 @@ def main(args):
 
     for scene_id, poses_scene in ests_org.items():
 
-        tpath_keys = dataset_params.scene_tpaths_keys(dp_split["eval_modality"], dp_split["eval_sensor"], scene_id)
-        scene_modality = dataset_params.get_scene_sensor_or_modality(dp_split["eval_modality"], scene_id)
-        scene_sensor = dataset_params.get_scene_sensor_or_modality(dp_split["eval_sensor"], scene_id)
+        tpath_keys = dataset_params.scene_tpaths_keys(
+            dp_split["eval_modality"], dp_split["eval_sensor"], scene_id
+        )
+        scene_modality = dataset_params.get_scene_sensor_or_modality(
+            dp_split["eval_modality"], scene_id
+        )
+        scene_sensor = dataset_params.get_scene_sensor_or_modality(
+            dp_split["eval_sensor"], scene_id
+        )
 
         # Create a new renderer if image size has changed
-        scene_width, scene_height = dataset_params.get_im_size(dp_split, scene_modality, scene_sensor)
+        scene_width, scene_height = dataset_params.get_im_size(
+            dp_split, scene_modality, scene_sensor
+        )
         if (width, height) != (scene_width, scene_height):
             width, height = scene_width, scene_height
             misc.log(f"Creating renderer of type {args.renderer_type}")
@@ -299,15 +330,27 @@ def main(args):
                 ren.add_object(obj_id, model_path, surf_color=model_color)
 
         # Load info and ground-truth poses for the current scene.
-        scene_camera = inout.load_scene_camera(dp_split[tpath_keys["scene_camera_tpath"]].format(scene_id=scene_id))
-        scene_gt = inout.load_scene_gt(dp_split[tpath_keys["scene_gt_tpath"]].format(scene_id=scene_id))
+        scene_camera = inout.load_scene_camera(
+            dp_split[tpath_keys["scene_camera_tpath"]].format(scene_id=scene_id)
+        )
+        scene_gt = inout.load_scene_gt(
+            dp_split[tpath_keys["scene_gt_tpath"]].format(scene_id=scene_id)
+        )
 
         split_type_str = " - " + split_type if split_type is not None else ""
-        misc.log(f"Visualizing pose estimates - method: {method}, dataset: {dataset}{split_type_str}, scene: {scene_id}")
-        
-        for im_ind, (im_id, poses_img) in enumerate(poses_scene.items()):
-            if args.mode == "gt":
+        misc.log(
+            f"Visualizing pose estimates - method: {method}, dataset: {dataset}{split_type_str}, scene: {scene_id}"
+        )
 
+        if args.mode == "gt":
+            # List of considered images.
+            if scene_im_ids is not None:
+                im_ids = scene_im_ids[scene_id]
+            else:
+                im_ids = sorted(scene_gt.keys())
+            if args.im_ids:
+                im_ids = set(im_ids).intersection(args.im_ids)
+            for im_counter, im_id in enumerate(im_ids):
                 # List of considered ground-truth poses.
                 gt_ids_curr = range(len(scene_gt[im_id]))
                 if args.gt_ids:
@@ -334,7 +377,8 @@ def main(args):
                             ],
                         }
                     )
-            else:
+        else:
+            for im_ind, (im_id, poses_img) in enumerate(poses_scene.items()):
 
                 im_ests_vis = []
                 im_ests_vis_obj_ids = []
@@ -368,11 +412,11 @@ def main(args):
                 # Join the per-object estimates to make it a single visual.
                 # if there are multiple estimates per object, they are treated as independent entries
                 poses = list(itertools.chain.from_iterable(im_ests_vis))
-        
+
         for im_ind, (im_id, poses_img) in enumerate(poses_scene.items()):
 
             # Retrieve camera intrinsics.
-            if dataset == 'hot3d':
+            if dataset == "hot3d":
                 cam = pose_error_htt.create_camera_model(scene_camera[im_id])
             else:
                 cam = scene_camera[im_id]["cam_K"]
@@ -383,7 +427,9 @@ def main(args):
                 # rgb_tpath is an alias refering to the sensor|modality image paths on which the poses are rendered
                 im_tpath = tpath_keys["rgb_tpath"]
                 # check for BOP classic (itodd)
-                rgb_available = dataset_params.sensor_has_modality(dp_split, scene_sensor, 'rgb')
+                rgb_available = dataset_params.sensor_has_modality(
+                    dp_split, scene_sensor, "rgb"
+                )
                 if im_tpath == "rgb_tpath" and not rgb_available:
                     im_tpath = "gray_tpath"
 
@@ -394,25 +440,35 @@ def main(args):
                 if rgb.ndim == 2:
                     rgb = np.dstack([rgb, rgb, rgb])
                 else:
-                    rgb = rgb[:,:,:3]  # should we keep this?
-                    
+                    rgb = rgb[:, :, :3]  # should we keep this?
+
             depth = None
             if args.vis_depth_diff or (args.vis_rgb and args.vis_rgb_resolve_visib):
-                depth_available = dataset_params.sensor_has_modality(dp_split, scene_sensor, "depth")
+                depth_available = dataset_params.sensor_has_modality(
+                    dp_split, scene_sensor, "depth"
+                )
                 if not depth_available:
-                    misc.log(f"{scene_sensor} has no depth data, skipping depth visualization")
+                    misc.log(
+                        f"{scene_sensor} has no depth data, skipping depth visualization"
+                    )
                     args.vis_depth_diff = False
                     args.vis_rgb_resolve_visib = False
                 else:
                     depth = inout.load_depth(
-                        dp_split[tpath_keys["depth_tpath"]].format(scene_id=scene_id, im_id=im_id)
+                        dp_split[tpath_keys["depth_tpath"]].format(
+                            scene_id=scene_id, im_id=im_id
+                        )
                     )
                     depth *= scene_camera[im_id]["depth_scale"]  # Convert to [mm].
 
             if args.mode == "gt":
 
                 # Path to the output RGB visualization.
-                split = "{}_{}".format(args.dataset_split, scene_sensor) if scene_sensor else args.dataset_split 
+                split = (
+                    "{}_{}".format(args.dataset_split, scene_sensor)
+                    if scene_sensor
+                    else args.dataset_split
+                )
                 vis_rgb_path = None
                 if args.vis_rgb:
                     vis_rgb_path = args.vis_rgb_tpath.format(
@@ -455,7 +511,7 @@ def main(args):
                         scene_id=scene_id,
                         vis_name=vis_name,
                     )
-                    
+
             visualization.vis_object_poses(
                 poses=poses_img,
                 K=cam,
