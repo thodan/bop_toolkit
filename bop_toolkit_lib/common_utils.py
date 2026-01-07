a77856929211d2478d6083394259bb08f3979e3a
kirilllzaitsev <kzaitsev88@gmail.com>
2026-01-08 00:00:59 +0100

    v:docs

--- bop_toolkit_lib/common_utils.py
+++ bop_toolkit_lib/common_utils.py
@@ -1,8 +1,14 @@
-
 import numpy as np
+from PIL import Image
 
 
 def adjust_img_for_plt(img):
+    """Converts an image to the HWC uint8 format. For example, used for plt.imshow().
+
+    :param img: Image in CHW or HWC format, numpy array or torch tensor. If a batch dimension exists, it must be 1.
+    :return: Image in HWC uint8 format.
+    """
+
     img = cast_to_numpy(img)
     if len(img.shape) == 4:
         if img.shape[0] == 1:
@@ -16,7 +22,14 @@ def adjust_img_for_plt(img):
     img = img.astype(np.uint8)
     return img
 
+
 def cast_to_numpy(x, dtype=None) -> np.ndarray:
+    """Casts input to numpy array of a given type.
+    
+    :param x: Input data. Can be a numpy array, a torch tensor, a PIL image, a list/tuple/dict with the aforementioned types.
+    :return: Numpy array or the same structure as input with numpy arrays.
+    """
+
     if x is None or isinstance(x, str):
         return x
     elif isinstance(x, list) or isinstance(x, tuple):
@@ -27,6 +40,11 @@ def cast_to_numpy(x, dtype=None) -> np.ndarray:
         if dtype is not None:
             x = x.astype(dtype)
         return x
+    elif isinstance(x, Image.Image):
+        arr = np.array(x)
+        if dtype is not None:
+            arr = arr.astype(dtype)
+        return arr
     elif isinstance(x, (int, float, complex, np.float32)):
         return x
     arr = x.detach().cpu().numpy()
